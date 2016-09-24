22ba45fed0651f4ebcb6786895ee71c3523a1d3e
Gaston Figueroa <figueroa@work.co>
2016-09-24 11:47:21 -0700

    Preven default on next/prev click.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,13 +4,13 @@
       <h2>Leg <span v-bind:class="fade ? 'fadeIn' : ''">{{ runs[selectedRun].leg }}</span></h2>
       <nav class="leg-nav">
         <ul>
-          <li v-on:click.stop="prevRun()">
-            <a href="#"><svg width="8px" height="11px" viewBox="0 0 8 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <li>
+            <a v-on:click.prevent="prevRun()" href="#"><svg width="8px" height="11px" viewBox="0 0 8 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
               <polygon points="7.1834532 9.9510064 6.4970211 10.5932203 0.8165468 5.2966102 6.4970211 0 7.1834532 0.6389036 2.1927271 5.2966102 7.1834532 9.9510064"></polygon>
             </svg></a>
           </li>
-          <li v-on:click.stop="nextRun()">
-            <a href="#"><svg width="8px" height="11px" viewBox="0 0 8 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <li>
+            <a v-on:click.prevent="nextRun()" href="#"><svg width="8px" height="11px" viewBox="0 0 8 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
               <polygon points="0.8165468 0.8456038 1.5029789 0.2033898 7.1834532 5.5 1.5029789 10.7966102 0.8165468 10.1577066 5.8072729 5.5"></polygon>
             </svg></a>
           </li>
